5f6b3a71a94d7e67cd4f41f0d3928b973d7d49ca
daniel <danieltoro.programacion@gmail.com>
2024-03-20 13:05:02 -0500

    como centrar un div

--- Backtesting-App-Trading/client/app/page.tsx
+++ Backtesting-App-Trading/client/app/page.tsx
@@ -11,26 +11,30 @@ export default function Home() {
   return (
     <div className={styled.container}>
 
-      <div className={styled.seccion_uno}>
+      <div className={styled.centralContainer}>
 
+          <div className={styled.seccion_uno}>
+
+          </div>
+
+          <div className={styled.seccion_dos}>
+
+            <div className={styled.enlaces}>
+              <Link href="/dashboard" className={styled.button}> Entrar </Link>
+              <Link href="/login" className={styled.button}> Login </Link>
+              <Link href="/newlogin" className={styled.button}> New Account </Link>
+            </div>
+
+            <div className={styled.inputsContainer}>
+              <input type="text" name="usuario" placeholder="usuario" className={styled.input}/>
+              <input type="password" name="usuario" placeholder="password" className={styled.input}/>
+              <input type="password" name="usuario" placeholder="Confirm password" className={styled.input}/>
+              <button className={styled.button}> Crear usuario </button>
+            </div>
+            
+          </div>
       </div>
-
-      <div className={styled.seccion_dos}>
-
-        <div className={styled.enlaces}>
-          <Link href="/dashboard" className={styled.button}> Entrar </Link>
-          <Link href="/login" className={styled.button}> Login </Link>
-          <Link href="/newlogin" className={styled.button}> New Account </Link>
-        </div>
-
-        <div className={styled.inputsContainer}>
-          <input type="text" name="usuario" placeholder="usuario" className={styled.input}/>
-          <input type="password" name="usuario" placeholder="password" className={styled.input}/>
-          <input type="password" name="usuario" placeholder="Confirm password" className={styled.input}/>
-          <button className={styled.button}> Crear usuario </button>
-        </div>
-        
-      </div>
+      
     </div>
   );
 }
